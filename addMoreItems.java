efff82e4bdbeff931ae1b4dd4f3b482f95a021f0
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-04-11 16:16:41 +0100

    Completed Semicolon Store

---
+++ addMoreItems.java
@@ -0,0 +1,15 @@
+public class AddMoreItems {
+	
+	public static String getUserName(String input) {
+
+	System.out.print("What did the buy ?");
+	String itemName = input.next();
+
+	System.out.print("How many pieces ?");
+	int piecesBought = input.nextInt();
+
+	System.out.print("How much per unit ?");
+	int perUnit = input.nextInt();
+	}
+}
+	
